180716d9b4138c8899e6e5e541a4450e0d40dc89
Minh Duc Nguyen <dekarvn@gmail.com>
2013-11-13 23:42:18 +1100

    delinked

--- application/views/activities.php
+++ application/views/activities.php
@@ -7,23 +7,24 @@
 				<h1 class="intro-text text-center">Activities</h1>
 				<hr>
 				<ul>
-					<li><a href="http://www.amazing-borneo.com/fishing/manukan-island-a-fun-fishing-trip-abk-zf01.html">MANUKAN ISLAND &amp; FUN FISHING TRIP</a></li>
-					<li><a href="http://www.amazing-borneo.com/daily-local-tour/kudat-the-northern-tip-of-borneo-abk-lt07.html">KUDAT - THE NORTHERN TIP OF BORNEO</a></li>
-					<li><a href="http://www.amazing-borneo.com/daily-local-tour/sunset-cruise-abk-s02.html">SUNSET CRUISE</a></li>
-					<li><a href="http://www.amazing-borneo.com/daily-local-tour/klias-wildlife-safari-river-cruise-proboscis-monkey-fireflies-abk-lt06.html">KLIAS WILDLIFE SAFARI RIVER CRUISE - PROBOSCIS MONKEY &amp; FIREFLIES</a></li>
-					<li><a href="http://www.amazing-borneo.com/daily-local-tour/mari-mari-cultural-village-tour-abk-lt04.html">MARI MARI CULTURAL VILLAGE TOUR</a></li>
-					<li><a href="http://www.e-borneo.com/travel/tours/eb-kk03.shtml">Islands Hopping with BBQ lunch</a></li>
-					<li><a href="http://www.amazing-borneo.com/daily-local-tour/white-water-rafting-padas-grade-3-4-abk-awr02.html">WHITE WATER RAFTING - PADA</a></li>
-					<li><a href="http://www.amazing-borneo.com/daily-local-tour/kinabalu-park-poring-hot-spring-tour-abk-kp01.html">KINABALU PARK &amp; PORING HOT SPRING TOUR</a> </li>
-					<li><a href="http://www.amazing-borneo.com/diving/mantanani-island-snorkeling-diving-abk-sd04.html">MANTANANI ISLAND – SNORKELING/DIVING</a></li>
+					<li>MANUKAN ISLAND &amp; FUN FISHING TRIP</li>
+					<li>KUDAT - THE NORTHERN TIP OF BORNEO</li>
+					<li>SUNSET CRUISE</li>
+					<li>KLIAS WILDLIFE SAFARI RIVER CRUISE - PROBOSCIS MONKEY &amp; FIREFLIES</li>
+					<li>MARI MARI CULTURAL VILLAGE TOUR</li>
+					<li>Islands Hopping with BBQ lunch</li>
+					<li>WHITE WATER RAFTING - PADA</li>
+					<li>KINABALU PARK &amp; PORING HOT SPRING TOUR </li>
+					<li>MANTANANI ISLAND – SNORKELING/DIVING</li>
 				</ul>
 
 			<div class="well">
-				<p><b>Contact Person:</b></p>
+				<b>Contact Person:</b><br>
 				<p>Lennie Lim</p>
 				<p><a href="mailto:lennie2111@gmail.com">lennie2111@gmail.com</a></p>
 				Whatsapp available: <i>0168055663</i>
 			</div>
+
 
 			</div>
 		</div>
